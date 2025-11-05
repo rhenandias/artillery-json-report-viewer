2f50a28fd7d1314add45bc2682c5015cb47d96dc
Rhenan Dias <diasrhenan@gmail.com>
2025-11-05 12:02:28 -0300

    Initial setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <div>
+      <h1 className="bg-red-200">Hello Vite</h1>
+      <Button>Olá Amigo</Button>
+    </div>
+  );
+}
+
+export default App;
